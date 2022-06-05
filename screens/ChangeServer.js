ca34ad16c3615a3f71c33c41eae4c90e6f79004a
anoda8 <nosada29@gmail.com>
2022-06-05 23:37:24 +0700

    server setting

---
+++ screens/ChangeServer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View } from 'react-native'
+
+export default function ChangeServer({route, navigation}) {
+    return (
+        <View>
+            <Text>Change Server</Text>
+        </View>
+    )
+}
